3799f22c9ea92ee3b69d407530b82934dc3f6f64
Micheal Wells <micheal.w.wells@gmail.com>
2024-09-04 14:38:43 -0700

    dir setup for fastapi and react-ts + capacitor

---
+++ app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'DDS-OfflineMap-POC',
+  webDir: 'dist'
+};
+
+export default config;
