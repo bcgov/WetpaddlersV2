2b25264fb5988c4618697070fed58185e6acd86d
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-09-17 14:36:58 -0700

    Consistent header gaps for containers

--- app/src/Components/Map/Map.style.ts
+++ app/src/Components/Map/Map.style.ts
@@ -2,7 +2,6 @@ import styled from '@emotion/styled';
 
 export const MapContent = styled.div`
   display: flex;
-  padding-top: 40pt;
-  width: 100vw;
-  height: calc(100vh - 40pt);
+  width: 100%;
+  height: 100vh;
 `;
