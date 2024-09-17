ce6003a39c76e8adc0a32519a64222231fad526a
Norris Ng <103449568+norrisng-bc@users.noreply.github.com>
2024-09-17 14:29:30 -0700

    Page for listing all DataBC data sets

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import Views from './interfaces/Views';
-import ComponentA from './Components/ComponentA/ComponentA';
+import DatasetList from './Components/DatasetList/DatasetList';
 import Map from './Components/Map/Map';
 import ComponentC from './Components/ComponentC/ComponentC';
 import ComponentD from './Components/ComponentD/ComponentD';
@@ -17,8 +17,8 @@ const App = () => {
   return (
     <AppContext.Provider value={{ contextExample, setContextExample }}>
       <Header toggleTab={handleViewChange} />
-      {tab === Views.A && <ComponentA />}
       {tab === Views.B && <Map />}
+      {tab === Views.DatasetList && <DatasetList />}
       {tab === Views.C && <ComponentC />}
       {tab === Views.D && <ComponentD />}
       {tab === Views.E && <ComponentE />}
