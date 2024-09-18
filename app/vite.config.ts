316885722deb768d1654942c134584b449382350
Robert Johnstone <rob@plasticviking.com>
2024-09-18 11:20:26 +0800

    vite cachedir

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  cacheDir: '.vite', //node_modules not writable on openshift
   plugins: [react()],
 })
