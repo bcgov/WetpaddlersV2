76ac13fc66610cbec008b38fd7fca63c8afc5028
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-09-18 07:20:12 -0700

    Change component A to Map in Views Enum

--- app/src/interfaces/Views.ts
+++ app/src/interfaces/Views.ts
@@ -1,9 +1,9 @@
 enum Views {
-  DatasetList = "DatasetList",
-  B = "Map",
-  C = "C",
-  D = "D",
-  E = "E",
+  DatasetList = 'DatasetList',
+  Map = 'Map',
+  C = 'C',
+  D = 'D',
+  E = 'E',
 }
 
 export default Views;
