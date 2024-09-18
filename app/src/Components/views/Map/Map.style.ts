acf3f624ccfac176a168afa4c46568b2ee68e942
Norris Ng <103449568+norrisng-bc@users.noreply.github.com>
2024-09-18 15:27:09 -0700

    Fix: map controls were partially obscured by header

--- app/src/Components/views/Map/Map.style.ts
+++ app/src/Components/views/Map/Map.style.ts
@@ -1,7 +1,12 @@
 import styled from '@emotion/styled';
+import { headerHeight } from '../../../assets/common-styles/common.styles';
 
 export const MapContent = styled.div`
   display: flex;
   width: 100%;
   height: 100vh;
+
+  .maplibregl-ctrl-top-left {
+    margin-top: ${headerHeight};
+  }
 `;
