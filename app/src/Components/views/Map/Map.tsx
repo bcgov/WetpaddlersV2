2d050780ebdd6cbc635b49e17d160a2375075214
Micheal Wells <micheal.w.wells@gmail.com>
2024-09-18 20:21:19 -0700

    pmtile the old way, opened issue on react-map-components-maplibre for pmtile protocol (no time to implement it now)

--- app/src/Components/views/Map/Map.tsx
+++ app/src/Components/views/Map/Map.tsx
@@ -1,19 +1,24 @@
-import { CommonFullCont } from '../../../assets/common-styles/common.styles';
-import maplibregl from 'maplibre-gl';
-import 'maplibre-gl/dist/maplibre-gl.css';
-import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
-import './map.css';
-import { useEffect, useRef, useState } from 'react';
-import { MapContent } from './Map.style';
 import MapboxDraw from '@mapbox/mapbox-gl-draw';
-import LayerPicker from '../../LayerPicker/LayerPicker';
-import { useDispatch, useSelector } from 'react-redux';
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
 import {
   MapLibreMap,
   MlMarker,
   MlWmsLayer,
+  useMap,
 } from '@mapcomponents/react-maplibre';
+<<<<<<< Updated upstream
 import WarningModal from '../../WarningModal/WarningModal';
+=======
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import { PMTiles, Protocol } from 'pmtiles';
+import { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { CommonFullCont } from '../../../assets/common-styles/common.styles';
+import LayerPicker from '../../LayerPicker/LayerPicker';
+import { MapContent } from './Map.style';
+import './map.css';
+>>>>>>> Stashed changes
 // @ts-expect-error Override
 MapboxDraw.constants.classes.CONTROL_BASE = 'maplibregl-ctrl';
 // @ts-expect-error Override
@@ -29,55 +34,60 @@ const Map = (props: any) => {
   const dispatch = useDispatch();
   const layerDict = useSelector((state: any) => state.MapState.layersDict);
   const mapCont = useRef<any>(null);
-  const map = useRef<any>(null);
+  const map = useMap({ mapId: 'map' });
   const drawTools = useRef<any>(new MapboxDraw());
   const [lat] = useState<number>(-121);
   const [long] = useState<number>(54);
   const marker = new maplibregl.Marker({
     element: positionMarkerEl,
   });
-  /*
-  useEffect(() => {
-    if (map.current) return;
-    map.current = new maplibregl.Map({
-      container: mapCont.current,
-      maxZoom: 18,
-      zoom: 3,
-      minZoom: 0,
-      center: [lat ?? -121, long ?? 54],
-      style: {
-        glyphs: 'https://fonts.openmaptiles.org/{fontstack}/{range}.pbf',
-        version: 8,
-        sources: {
-          'Esri-Sat-LayerHD': {
-            type: 'raster',
-            tiles: [
-              'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-            ],
-            tileSize: 256,
-            maxzoom: 18,
-          },
-        },
-        layers: [
-          {
-            id: 'Esri-Sat-LayerHD',
-            type: 'raster',
-            source: 'Esri-Sat-LayerHD',
-            minzoom: 0,
-          },
-        ],
-      },
-    }).addControl(drawTools.current, 'top-left');
 
-    marker.setLngLat([lat, long]);
-    marker.addTo(map.current);
-  });
   useEffect(() => {
-    marker.remove();
-    marker.setLngLat([lat, long]);
-    marker.addTo(map.current);
-  }, [lat, long]);
-    */
+    const protocol = new Protocol();
+    maplibregl.addProtocol('pmtiles', (request) => {
+      return new Promise((resolve, reject) => {
+        const callback = (err, data) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve({ data });
+          }
+        };
+        protocol.tile(request, callback);
+      });
+    });
+
+    const PMTILES_URL = `https://nrs.objectstore.gov.bc.ca/uphjps/invasives-local.pmtiles`;
+
+    const p = new PMTiles(PMTILES_URL);
+
+    // this is so we share one instance across the JS code and the map renderer
+    protocol.add(p);
+
+    // we first fetch the header so we can get the center lon, lat of the map.
+    p.getHeader().then((header) => {
+      map?.map?.addSource('pmtiles', {
+        type: 'vector',
+        url: `pmtiles://${PMTILES_URL}`,
+        //              url: `https://nrs.objectstore.gov.bc.ca/uphjps/invasives-local.pmtiles`,
+        // url: `pmtiles://${ CONFIG.PUBLIC_MAP_URL}`,
+        attribution: '© <a href="https://openstreetmap.org">OpenStreetMap</a>',
+      });
+      map?.map?.addLayer({
+        id: 'invasivesbc-pmtile-vector',
+        type: 'circle',
+        source: 'pmtiles',
+        'source-layer': 'invasives',
+        layout: {
+          visibility: 'visible',
+        },
+        paint: { 'circle-color': '#0905f5', 'circle-opacity': 0.5 },
+        maxzoom: 20,
+      });
+    });
+
+  }, [map]);
+
   return (
     <CommonFullCont>
       <WarningModal />
